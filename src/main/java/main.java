69bb601c1f56656132161fb2aa1c31f36df137fa
Erryon <42776200+Erryon@users.noreply.github.com>
2021-02-22 23:33:36 +0100

    [INFO] ajout des commentaires

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,5 +1,13 @@
-public class main {
+/**
+ * Class Main du début de projet
+ */
+public class Main {
+    /**
+     * méthode main Hello world
+     * @param args
+     */
     public static void main (String[] args){
         System.out.println("Hello World");
     }
+
 }
